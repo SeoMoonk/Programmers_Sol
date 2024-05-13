26fad080d8eddb65ded5a6eb3d44ac578d1bc31c
SeoMoonk <sunghoon1216@naver.com>
2024-05-13 14:44:26 +0900

    refactor: BOJ2559 코드ã 수정

--- src/main/java/bj/BJ2559.java
+++ src/main/java/bj/BJ2559.java
@@ -3,26 +3,43 @@ package bj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BJ2559 {
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N;       //온도를 측정한 전체 날짜의 수
+    static int K;       //합을 구하기 위한 연속적인 날짜의 수
+    static int[] arr;   //매일 측정한 온도
+    static int MAX_TEMP = -10000001;
 
     public static void main(String[] args) throws IOException {
 
-        String firstLine = br.readLine();
-        int N = Integer.parseInt(firstLine.split(" ")[0]);
-        int K = Integer.parseInt(firstLine.split(" ")[1]);
-
-        int[] arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for(int i=0; i<N; i++){
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i=0; i<N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-    }
+        int temp = 0;
 
+        for(int i=0; i<K; i++) {
+            temp += arr[i];
+        }
+
+        MAX_TEMP = Math.max(MAX_TEMP, temp);
+
+        for(int i=K; i<N; i++) {
+            temp = temp - arr[i-K] + arr[i];
+            MAX_TEMP = Math.max(MAX_TEMP, temp);
+        }
+
+        System.out.println(MAX_TEMP);
+    }
 }
