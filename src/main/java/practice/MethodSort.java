d3c0cd4b905f1528bc9e0723565e96b9a19e78f6
SeoMoonk <sunghoon1216@naver.com>
2024-05-17 15:49:43 +0900

    prac: 메서드 정렬

---
+++ src/main/java/practice/MethodSort.java
@@ -0,0 +1,51 @@
+package practice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class MethodSort {
+
+    static int[] nums = {7, 3, 1, 4, 2};
+    static Integer[] nums2 = {7, 3, 1, 4, 2};
+    static String[] strNums = {"7", "5", "8", "2", "3"};
+    static String[] strWords = {"car", "banana", "apple", "box"};
+
+    static ArrayList<Integer> numList = new ArrayList<>(Arrays.asList(3, 6, 1, 2, 4));
+    static ArrayList<String> strList = new ArrayList<>(Arrays.asList("7", "14", "1", "3", "5"));
+    static ArrayList<String> wordList = new ArrayList<>(Arrays.asList("cat", "apple", "dog", "apex", "cap"));
+
+    public static void main(String[] args) {
+
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+
+        Arrays.sort(nums2, Collections.reverseOrder());     // 반대 정렬을 원하면 Integer[]로 선언해야함.
+        System.out.println(Arrays.toString(nums2));
+
+
+        Arrays.sort(strNums);
+        System.out.println(Arrays.toString(strNums));
+
+        Arrays.sort(strNums, Collections.reverseOrder());
+        System.out.println(Arrays.toString(strNums));
+
+
+        Arrays.sort(strWords);
+        System.out.println(Arrays.toString(strWords));
+
+        Arrays.sort(strWords, Collections.reverseOrder());
+        System.out.println(Arrays.toString(strWords));
+
+        numList.sort(Comparator.naturalOrder());
+        System.out.println(numList);
+
+        strList.sort(Comparator.naturalOrder());
+        System.out.println(strList);
+
+        wordList.sort(Comparator.naturalOrder());
+        System.out.println(wordList);
+    }
+
+}
