9557579b969ecd97e08e7b550ad19ede70b212af
SeoMoonk <sunghoon1216@naver.com>
2024-05-17 21:42:55 +0900

    BOJ15654_N과 M(5)

---
+++ src/main/java/bj/BJ15654.java
@@ -0,0 +1,60 @@
+package bj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BJ15654 {
+
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N, M;
+    static int[] arr;
+    static int[] sel;
+    static boolean[] check;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        sel = new int[M];
+        check = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        perm(0);
+        System.out.println(sb.toString());
+    }
+
+    static void perm(int depth) {
+
+        if(depth == M) {
+            for(int i=0; i<M; i++) {
+                sb.append("%d ".formatted(sel[i]));
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=0; i<N; i++) {
+            if(!check[i]) {
+                check[i] = true;
+                sel[depth] = arr[i];
+                perm(depth+1);
+                check[i] = false;
+            }
+        }
+    }
+}
