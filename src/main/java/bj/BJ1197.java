597a746d1f2544e1b4bb9b2a868269151d987189
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2024-05-24 02:11:22 +0900

    BJ1197_복습

--- src/main/java/bj/BJ1197.java
+++ src/main/java/bj/BJ1197.java
@@ -12,10 +12,80 @@ public class BJ1197 {
     static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
     static int V, E;
-    static List<Edge> edges = new ArrayList<>();
     static int[] p;
     static int[] r;
-    static int totalWeight = 0, count = 0;
+    static List<Edge> edges = new ArrayList<>();
+    static int sum = 0;
+    static int count = 0;
+
+    public static void main(String[] args) throws IOException {
+
+        st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+
+        p = new int[V + 1];
+        r = new int[V + 1];
+
+        for (int i = 0; i < V + 1; i++) {
+            p[i] = i;
+        }
+
+        for (int i = 0; i < E; i++) {
+
+            st = new StringTokenizer(br.readLine());
+
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            edges.add(new Edge(start, end, weight));
+        }
+
+        edges.sort(Edge::compareTo);
+
+        for (Edge edge : edges) {
+
+            int start = findParent(edge.start);
+            int end = findParent(edge.end);
+            int weight = edge.weight;
+
+            if (start != end) {
+                union(start, end);
+                sum += weight;
+                count++;
+            }
+
+            if (count == V - 1) {
+                break;
+            }
+        }
+
+        System.out.println(sum);
+
+    }
+
+    static int findParent(int x) {
+
+        if (p[x] != x) {
+            p[x] = findParent(p[x]);
+        }
+
+        return p[x];
+    }
+
+    static void union(int x, int y) {
+
+        if (r[x] > r[y]) {
+            p[y] = x;
+        } else {
+            p[x] = y;
+            if (r[x] == r[y]) {
+                r[y]++;
+            }
+        }
+
+    }
 
     static class Edge implements Comparable<Edge> {
 
@@ -34,75 +104,4 @@ public class BJ1197 {
             return this.weight - o.weight;
         }
     }
-
-    public static void main(String[] args) throws IOException {
-
-        st = new StringTokenizer(br.readLine());
-
-        V = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
-
-        p = new int[V+1];
-        r = new int[V+1];
-
-        for(int i=0; i<V+1; i++) {
-            p[i] = i;
-        }
-
-        for(int i=0; i<E; i++) {
-
-            st = new StringTokenizer(br.readLine());
-
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-
-            edges.add(new Edge(start, end, weight));
-        }
-
-        edges.sort(Edge::compareTo);
-
-        for(Edge edge : edges) {
-
-            int startRoot = find(edge.start);
-            int endRoot = find(edge.end);
-
-            if (startRoot != endRoot) {  // 최종 부모가 다르면?
-                union(startRoot, endRoot);  // Union 가능!
-
-                totalWeight += edge.weight;  // mst에 현재 가중치를 추가하고
-                count += 1;  // 간선을 하나 소진했음을 나타냄
-            }
-
-            if (count == V - 1) {  // 트리의 갯수만큼 뽑았다?
-                break; // 알고리즘 종료!
-            }
-        }
-
-        System.out.println(totalWeight);
-    }
-
-    static int find(int x) {
-
-        if(p[x] != x) {
-            p[x] = find(p[x]);
-        }
-
-        return p[x];
-    }
-
-    static void union(int x, int y) {
-
-        if(r[x] > r[y]) {
-            p[y] = p [x];
-        } else {
-            p[x] = p[y];
-
-            if(r[x] == r[y]) {
-                r[y]++;
-            }
-        }
-
-    }
-
 }
