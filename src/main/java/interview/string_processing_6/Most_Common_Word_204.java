8cd47bf131d110f2a0b7d599dcba095e0dd30479
SeoMoonk <sunghoon1216@naver.com>
2024-09-24 14:56:09 +0900

    단어 지정 정규식, Map 최댓값 Collection 메서드

--- src/main/java/interview/string_processing_6/Most_Common_Word_204.java
+++ src/main/java/interview/string_processing_6/Most_Common_Word_204.java
@@ -1,12 +1,8 @@
 package interview.string_processing_6;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.*;
 
 public class Most_Common_Word_204 {
-
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) {
 
@@ -18,25 +14,26 @@ public class Most_Common_Word_204 {
         System.out.println(answer);
     }
 
-    static String mostCommonWord(String p, String[] banned) {
-
-        Set<String> ban = new HashSet<>(Arrays.asList(banned));
-        Map<String, Integer> counts = new HashMap<>();
-
-        String[] words = p.replaceAll("\\W+", " ").toLowerCase().split(" ");
-
-        for(String w : words) {
-            if(!ban.contains(w)) {
-                counts.put(w, counts.getOrDefault(w, 0) +1);
-            }
-        }
-
-        return Collections.max(counts.entrySet(), Map.Entry.comparingByValue()).getKey();
-    }
 
     /*
     금지된 단어를 제외하고 가장 흔하게 등장하는 단어를 출력하라.
     대소문자를 구분하지 않으며, 구두점(마침표, 쉼표 등) 또한 무시한다.
      */
+    static String mostCommonWord(String p, String[] banned) {
 
+        Set<String> ban = new HashSet<>(Arrays.asList(banned));
+        Map<String, Integer> counts = new HashMap<>();
+
+        // \W+ -> 연속적으로(+) 단어 문자(Word Character)가 아닌 것을 지정하는 정규식 -> 공백으로 치환(replaceAll)
+        String[] words = p.replaceAll("\\W+", " ").toLowerCase().split(" ");
+
+        for (String w : words) {
+            if (!ban.contains(w)) {
+                counts.put(w, counts.getOrDefault(w, 0) + 1);
+            }
+        }
+
+        //Map(Key-Value)에서 가장 큰 값을 찾는다. 기준은 값(Value)으로. 최종적으로 Key(단어)를 반환한다.
+        return Collections.max(counts.entrySet(), Map.Entry.comparingByValue()).getKey();
+    }
 }
