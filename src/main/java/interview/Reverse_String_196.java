870eb22e139fdc56841868fc31a825e7cb25cab1
SeoMoonk <sunghoon1216@naver.com>
2024-09-24 14:27:50 +0900

    Reverse_String 196

---
+++ src/main/java/interview/Reverse_String_196.java
@@ -0,0 +1,34 @@
+package interview;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Reverse_String_196 {
+
+    public static void main(String[] args) throws IOException {
+
+        char[] str = {'r', 'a', 'c', 'e', 'c', 'a', 'R'};
+
+        char[] reverse = reverse(str);
+
+        System.out.println(Arrays.toString(reverse));
+    }
+
+    static char[] reverse(char[] s) {
+
+        int start = 0;
+        int end = s.length-1;
+
+        while(start < end) {
+
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+
+            start++;
+            end--;
+        }
+
+        return s;
+    }
+}
