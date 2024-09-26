aa2e54d1c6e13c8399abef7830dda63e91d22197
SeoMoonk <sunghoon1216@naver.com>
2024-09-26 15:26:38 +0900

    feat: 빗물 트래핑(2) 스택 방식

--- src/main/java/interview/array/Trapping_Rain_Water_42.java
+++ src/main/java/interview/array/Trapping_Rain_Water_42.java
@@ -1,5 +1,7 @@
 package interview.array;
 
+import java.util.ArrayDeque;
+
 public class Trapping_Rain_Water_42 {
 
     //높이를 입력받아 비 온 후 얼마나 많은 물이 쌓일 수 있는지 계산하라.
@@ -7,9 +9,11 @@ public class Trapping_Rain_Water_42 {
 
         int[] height = {1, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
 
-        int answer = trap1(height);
+        int answer = trap1(height);     //투 포인터 방식 풀이
+        int answer2 = trap2(height);    //스택 방식 풀이
 
         System.out.println("answer = " + answer);
+        System.out.println("answer2 = " + answer2);
     }
 
     public static int trap1(int[] height) {
@@ -21,6 +25,7 @@ public class Trapping_Rain_Water_42 {
         int rightMax = height[right];
 
         //투 포인터가 서로 겹칠 때까지 반복
+        // -> 좌우 어느 쪽이든 낮은 쪽은 높은 쪽을 향해서 포인터가 가운데로 점점 이동한다.
         while(left < right) {
 
             leftMax = Math.max(height[left], leftMax);
@@ -37,6 +42,35 @@ public class Trapping_Rain_Water_42 {
                 right--;
             }
         }
+        return volume;
+    }
+
+    static int trap2(int[] height) {
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        int volume = 0;
+
+        for(int i=0; i<height.length; i++) {
+            //변곡점을 만나는 경우(더 큰 것을 만나는 경우)
+            while(!stack.isEmpty() && height[i] > height[stack.getLast()]) {
+                //스택에서 꺼낸다(pop)
+                Integer top = stack.removeLast();
+
+                if(stack.isEmpty())
+                    break;
+
+                //스택의 마지막 위치까지를 거리로 계산
+                int distance = i - stack.getLast() - 1;
+                //현재 높이 또는 스택의 마지막 위치 높이 중 낮은 값에 방금 꺼낸 높이의 차이를 물 높이로 지정
+                int waters = Math.min(height[i], height[stack.getLast()]) - height[top];
+                //물이 쌓이는 양은 거리와 물 높이의 곱
+                volume += distance * waters;
+            }
+
+            //진행하면서 현재 위치를 스택에 삽입
+            stack.addLast(i);
+        }
+
         return volume;
     }
 }
